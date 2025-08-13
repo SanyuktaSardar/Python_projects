9ee06eb012d465ce7e43cd2b7e46ce93cd5f807a
Sanyukta Sardar <ssanyukta16@gmail.com>
2025-08-13 19:26:46 +0530

    Update main.py

--- Workout-tracker/main.py
+++ Workout-tracker/main.py
@@ -2,13 +2,6 @@ import requests
 from datetime import datetime
 import os
 
-os.environ['APPLICATION_ID'] = "950ace28"
-os.environ["APP_KEY"] = "3723fe0e72788c38c2282e3f87338976"
-os.environ["APP_ENDPOINT" ]= "https://trackapi.nutritionix.com//v2/natural/exercise"
-
-#SHEETY API
-os.environ['SHEETY_ENDPOINT'] = "https://api.sheety.co/95a22f2644d79067a976fd7bafc58c88/myWorkouts/workouts"
-os.environ['TOKEN'] = "c2FueXVrdGE6c2FuanUxMzI="
 #TODO: FIGURE OUT HOW TO PRINT EXERCISE STATS FOR PLAIN TEXT INPUT
 #EXERCISE API
 APPLICATION_ID = os.environ.get('APPLICATION_ID')
@@ -56,5 +49,6 @@ for exercise in exercise_list:
     response = requests.post(SHEETY_ENDPOINT,json=body,headers=header)
     print(response.raise_for_status())
     # print(response.json())
+
 
 # {'date': '21/07/2020', 'time': '15:00:00', 'exercise': 'Running', 'duration': 22, 'calories': 130, 'id': 2}
