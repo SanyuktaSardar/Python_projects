877c50fbdbee0acf10afa7d5caca34b33df25518
Sanyukta Sardar <ssanyukta16@gmail.com>
2025-08-25 20:25:22 +0530

    Add files via upload

---
+++ day_16_start/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle,Screen
+
+import prettyTables
+
+import another_module
+
+print(another_module.another_variable)
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+#create an object
+my_screen = Screen()
+#use attribute
+print(my_screen.canvheight)
+
+#use function
+timmy.forward(100)
+my_screen.exitonclick()
+
+
+
+
